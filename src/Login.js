bf5806d06c1538720e8ab3a2f092aad062fb506a
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-11-22 23:32:43 +0000

    rest of signin form

--- src/Login.js
+++ src/Login.js
@@ -20,32 +20,65 @@ const Login = () => {
         setErrMsg(''); // alaways clean err message
     }, [user, pwd]);
 
+    // handle submit function
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        // clear and password field once submitted
+        setPwd("");
+        setSuccess(true);
+    };
+
     return (
-        <section className='app-form'>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
-                aria-live="assertive">{errMsg}</p>
-            <h1>Sign in</h1>
-            <form className='labelField'>
-                <label typeof='username'>Username:</label>
-                <input id='username' ref={userRef}
-                    autoComplete='off'
-                    onChange={(e) => {
-                        setUser(e.target.value)
-                    }}
-                    value={user}
-                    required />
-                <label typeof='password'>Password:</label>
-                <input type="text"
-                    id="password"
-                    autoComplete='off'
-                    onChange={(e) => {
-                        setPwd(e.target.value)
-                    }}
-                    value={pwd}
-                    required/>
-            </form>
-        </section>
-    )
+        <div>
+            {success ? (
+                <section className='app-form'>
+                    <h1> Welcome {user} !</h1>
+                    <br />
+                    <p>Go to <a href="#"> Home</a></p>
+                </section>
+
+            ) : (
+                <section className='app-form'>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
+                        aria-live="assertive">{errMsg}</p>
+                    <h1>Sign in</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="labelField">
+                            <label typeof='username'>Username:</label>
+                            <input id='username' ref={userRef}
+                                autoComplete='off'
+                                onChange={(e) => {
+                                    setUser(e.target.value)
+                                }}
+                                value={user}
+                                required />
+                        </div>
+                        <div className="labelField">
+                            <label typeof='password'>Password:</label>
+                            <input type="password"
+                                id="password"
+                                onChange={(e) => {
+                                    setPwd(e.target.value)
+                                }}
+                                value={pwd}
+                                required />
+                        </div>
+                        <div className="labelField">
+                            <button>Sign In</button>
+                        </div>
+                        <div className="labelField">
+                            <div className="signin">
+                                <p>Don't have an account? </p>
+                                <p><a href="#">Sign up</a> Here!</p>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+            )}
+        </div>
+
+    );
 
 
 }
