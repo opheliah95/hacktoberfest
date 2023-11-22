cf28e915adfac842f74b23ce78a8ba5b27c78a2d
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2023-11-22 00:30:04 +0000

    broken for now, fix later

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 
 const Login = () => {
@@ -7,33 +7,41 @@ const Login = () => {
 
     // list of states
     const [user, setUser] = useState('');
-    const[pwd, setPwd] = useState('');
-    const[errMsg, setErrMsg] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
     // this runs on every render
-    useEffect(()=>{
+    useEffect(() => {
         userRef.current.focus();
     }, []);
 
-    useEffect(()=> {
+    useEffect(() => {
         setErrMsg(''); // alaways clean err message
     }, [user, pwd]);
 
     return (
         <section>
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
-            aria-live="assertive">{errMsg}</p>
+                aria-live="assertive">{errMsg}</p>
             <h1>Sign in</h1>
             <form>
-                
+                <label typeof='username'>Username:</label>
+                <input id='username' ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => {
+                        setUser(e.target.value)
+                    }}
+                    value={user}
+                    required />
+
             </form>
         </section>
     )
 
 
 }
-    
+
 
 
 export default Login;
